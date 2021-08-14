60448835df12e6fd61f1f18659111be163febb9a
dtaylor-us <derektaylor.us@gmail.com>
2021-08-14 15:20:14 -0500

    add start

--- src/server.js
+++ src/server.js
@@ -45,13 +45,12 @@ app.use('/api/v1/', api);
 app.use(cors());
 
 // This middleware informs the express application to serve our compiled React files
-if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
     app.use(express.static(path.join(__dirname, 'client/build')));
 
     app.get('*', function (req, res) {
         res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     });
-};
+
 
 // Catch any bad requests
 app.get('*', (req, res) => {
